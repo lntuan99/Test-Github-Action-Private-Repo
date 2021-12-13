fd797a5f7f997912dd343b1340fab18cfba8d2ac
tuannho <lntuan99@gmail.com>
2021-12-14 00:05:13 +0700

    pls

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pwd
 # Copy service file, incase if there are any changes
 cp test-cicd.service /etc/systemd/system/test-cicd.service
 # reload configurations incase if service file has changed
