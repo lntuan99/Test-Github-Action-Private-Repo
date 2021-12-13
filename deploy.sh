fc8a99dc23e647f909216adbb2d092d63ae9b768
tuannho <lntuan99@gmail.com>
2021-12-14 03:34:14 +0700

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-pwd
 # Copy service file, incase if there are any changes
-cp test-cicd.service /etc/systemd/system/test-cicd2.service
+cp test-cicd.service /etc/systemd/system/test-cicd.service
 
-systemctl enable test-cicd2.service
+systemctl enable test-cicd.service
 # reload configurations incase if service file has changed
 systemctl daemon-reload
 # restart the service
-systemctl restart test-cicd2
+systemctl restart test-cicd
